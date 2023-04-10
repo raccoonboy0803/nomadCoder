eee5a5c2085f3bf154e2f3ba9ba9fdfb34ca22eb
raccoonboy <raccoonboy0803@gmail.com>
2023-04-10 18:13:14 +0900

    #6 EFFECTS complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import Button from './Button';
 import styles from './App.module.css';
+import { useState, useEffect } from 'react';
+
+function Hello() {
+  function byeFn() {
+    console.log('bye :(');
+  }
+
+  function hiFn() {
+    console.log('created :)');
+    return byeFn;
+  }
+  useEffect(hiFn, []);
+  return <h1>Hello</h1>;
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {
+    setShowing((prev) => !prev);
+  };
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={'Continue'} />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? 'Hide' : 'Show'}</button>
     </div>
   );
 }
